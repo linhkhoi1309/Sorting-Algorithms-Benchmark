c81edb063eb34742ab46b69f90be2c4cabfd8c6b
linhkhoi1309 <luongkhoi3456@gmail.com>
2024-06-18 13:59:33 +0700

    Update prototypes

---
+++ source/Sort.h
@@ -0,0 +1,13 @@
+#pragma once
+
+void selectionSort(int arr[], int n, long long& count_compare);
+void insertionSort(int arr[], int n, long long& count_compare);
+void bubbleSort(int arr[], int n, long long& count_compare);
+void shakerSort(int arr[], int n, long long& count_compare);
+void shellSort(int arr[], int n, long long& count_compare);
+void heapSort(int arr[], int n, long long& count_compare);
+void mergeSort(int arr[], int n, long long& count_compare);
+void quickSort(int arr[], int n, long long& count_compare);
+void countingSort(int arr[], int n, long long& count_compare);
+void radixSort(int arr[], int n, long long& count_compare);
+void flashSort(int arr[], int n, long long& count_compare);
