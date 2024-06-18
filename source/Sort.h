d632db58519aaf716cc199891e3ae4c1493c3185
linhkhoi1309 <luongkhoi3456@gmail.com>
2024-06-18 15:13:07 +0700

    Sort.h

--- source/Sort.h
+++ source/Sort.h
@@ -1,13 +1,34 @@
 #pragma once
 
+void selectionSort(int arr[], int n);
 void selectionSort(int arr[], int n, long long& count_compare);
+
+void insertionSort(int arr[], int n);
 void insertionSort(int arr[], int n, long long& count_compare);
+
+void bubbleSort(int arr[], int n);
 void bubbleSort(int arr[], int n, long long& count_compare);
+
+void shakerSort(int arr[], int n);
 void shakerSort(int arr[], int n, long long& count_compare);
+
+void shellSort(int arr[], int n);
 void shellSort(int arr[], int n, long long& count_compare);
+
+void heapSort(int arr[], int n);
 void heapSort(int arr[], int n, long long& count_compare);
+
+void mergeSort(int arr[], int n);
 void mergeSort(int arr[], int n, long long& count_compare);
+
+void quickSort(int arr[], int n);
 void quickSort(int arr[], int n, long long& count_compare);
+
+void countingSort(int arr[], int n);
 void countingSort(int arr[], int n, long long& count_compare);
+
+void radixSort(int arr[], int n);
 void radixSort(int arr[], int n, long long& count_compare);
+
+void flashSort(int arr[], int n);
 void flashSort(int arr[], int n, long long& count_compare);
